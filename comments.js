2e6a471f99b0e7ef0906bd03b61078a14ee0627d
Devin D-T <devindaotse@gmail.com>
2020-12-08 20:38:57 -0500

    add comment routing and testing middleware

---
+++ comments.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const { v4: uuid } = require('uuid')
+const router = express.Router()
+
+let comments = [
+    {
+        id:uuid(),
+        username: 'use1',
+        comment: 'sup ppl'
+    },
+    {
+        id:uuid(),
+        username: 'use2',
+        comment: 'sup use1'
+    },
+    {
+        id: uuid(),
+        username: 'use3',
+        comment: 'sup guys'
+    }
+]
+
+function log(req, res, next){
+    console.log('requesting comment starting page')
+    next()
+}
+// function sup(req, res){
+//     console.log('LOGGING')
+//     res.end()
+// }
+
+// router.get('/', log)
+router.get('/', log,(req, res)=>{
+    res.render('comments/index', {comments})
+})
+
+router.get('/new',(req, res)=>{
+    res.render('comments/new')
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/show', {comment})
+})
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params
+    const newComment = req.body.newcomment
+    const updateComment = comments.find(c => c.id === id)
+    updateComment.comment =  newComment
+    res.redirect('/comments')
+})
+
+router.get('/:id/edit', (req, res) => {
+    const{id} = req.params
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/edit', {comment})
+})
+
+router.post('/',(req, res) =>{
+    const { username, comment } = req.body
+    comments.push({id: uuid(), username, comment})
+    res.redirect('/comments')
+})
+
+router.delete('/:id',(req, res)=>{
+    const {id} = req.params
+    comments = comments.filter(c => c.id !== id)
+    res.redirect('/comments')
+
+})
+
+
+module.exports = router
+
+
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const cors = require('cors')
 const { v4: uuid } = require('uuid')
+const commentRoute = require('./comments.js')
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'))
@@ -15,70 +17,17 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false}))
 
 
-app.get('/',(req, res)=>{
+function log(req, res, next){
+    console.log('LOGGING on home page')
+    next()
+}
+
+
+app.get('/',log,(req, res)=>{
     res.render('start', { home: 'HOME PAGE'})
 })
 
-let comments = [
-    {
-        id:uuid(),
-        username: 'use1',
-        comment: 'sup ppl'
-    },
-    {
-        id:uuid(),
-        username: 'use2',
-        comment: 'sup use1'
-    },
-    {
-        id: uuid(),
-        username: 'use3',
-        comment: 'sup guys'
-    }
-]
-
-
-app.get('/comments',(req, res)=>{
-    res.render('comments/index', {comments})
-})
-
-app.get('/comments/new',(req, res)=>{
-    res.render('comments/new')
-})
-
-app.get('/comments/:id', (req, res) => {
-    const { id } = req.params
-    const comment = comments.find(c => c.id === id)
-    res.render('comments/show', {comment})
-})
-
-app.patch('/comments/:id', (req, res) => {
-    const { id } = req.params
-    const newComment = req.body.newcomment
-    const updateComment = comments.find(c => c.id === id)
-    updateComment.comment =  newComment
-    res.redirect('/comments')
-})
-
-
-app.get('/comments/:id/edit', (req, res) => {
-    const{id} = req.params
-    const comment = comments.find(c => c.id === id)
-    res.render('comments/edit', {comment})
-})
-
-app.post('/comments',(req, res) =>{
-    const { username, comment } = req.body
-    comments.push({id: uuid(), username, comment})
-    res.redirect('/comments')
-})
-
-app.delete('/comments/:id',(req, res)=>{
-    const {id} = req.params
-    comments = comments.filter(c => c.id !== id)
-    res.redirect('/comments')
-
-})
+app.use('/comments', commentRoute)
 
 app.post('/form',(req, res)=>{
     let {meat, qty} = req.body
@@ -86,8 +35,70 @@ app.post('/form',(req, res)=>{
     res.render('form', {msg})
 })
 
-
-
 app.listen(3000, ()=>{
     console.log("Listening on port 3000")
 })
+
+
+
+// let comments = [
+//     {
+//         id:uuid(),
+//         username: 'use1',
+//         comment: 'sup ppl'
+//     },
+//     {
+//         id:uuid(),
+//         username: 'use2',
+//         comment: 'sup use1'
+//     },
+//     {
+//         id: uuid(),
+//         username: 'use3',
+//         comment: 'sup guys'
+//     }
+// ]
+
+
+// app.get('/comments',(req, res)=>{
+//     res.render('comments/index', {comments})
+// })
+
+// app.get('/comments/new',(req, res)=>{
+//     res.render('comments/new')
+// })
+
+// app.get('/comments/:id', (req, res) => {
+//     const { id } = req.params
+//     const comment = comments.find(c => c.id === id)
+//     res.render('comments/show', {comment})
+// })
+
+// app.patch('/comments/:id', (req, res) => {
+//     const { id } = req.params
+//     const newComment = req.body.newcomment
+//     const updateComment = comments.find(c => c.id === id)
+//     updateComment.comment =  newComment
+//     res.redirect('/comments')
+// })
+
+
+// app.get('/comments/:id/edit', (req, res) => {
+//     const{id} = req.params
+//     const comment = comments.find(c => c.id === id)
+//     res.render('comments/edit', {comment})
+// })
+
+// app.post('/comments',(req, res) =>{
+//     const { username, comment } = req.body
+//     comments.push({id: uuid(), username, comment})
+//     res.redirect('/comments')
+// })
+
+// app.delete('/comments/:id',(req, res)=>{
+//     const {id} = req.params
+//     comments = comments.filter(c => c.id !== id)
+//     res.redirect('/comments')
+
+// })
+
