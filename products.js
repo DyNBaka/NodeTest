f79e4648132ddb07f62f11b84aa66e5fa5018961
Devin Dao-Tse <devindaotse@gmail.com>
2020-12-14 15:39:17 -0500

    need to finish edit routes

--- products.js
+++ products.js
@@ -12,11 +12,42 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, 
 
 router.get('/',async (req, res)=>{
     const products = await Product.find({})
-    // console.log(products)
     res.render('products/index', {products})
 })
 
+router.get('/new', (req,res) =>{
+  res.render('products/add')
+})
 
+router.get('/:id',async (req, res)=>{
+  const { id } = req.params
+  const product = await Product.findById(id)
+  console.log(product)
+  res.render('products/details', {product})
+  // res.send('Details page')
+})
+
+router.post('/', async (req, res)=>{
+  console.log(req.body)
+  const newProduct = new Product(req.body)
+  await newProduct.save()
+  res.redirect('/products')
+})
+
+
+router.get('/:id/edit',async(req, res)=>{
+  const {id} = req.params
+  const product = Product.findById(id)
+  res.render('products/edit', { product })
+})
+
+router.put('/products/:id', async(req, res)=>{
+  const {id} = req.params
+  // const product = await Product.findByIdAndUpdate(id, req.body)
+  console.log(req.body)
+  res.send('PUT SUCCESS')
+
+})
 
 
 
